ef2a11c71bb0712ebe242964a6164c4ff9ce3b69
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-06-14 07:12:37 -0700

    last fix

--- commands/player.py
+++ commands/player.py
@@ -3,14 +3,14 @@ from discord.ext import commands
 import aiohttp
 import json
 
+
 class Player(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.command(aliases=['profile', 'player'])
-    async def p(self, ctx, input = ""):
+    async def p(self, ctx, input=""):
         if input:
             player = input.replace(" ", "%20")
             print(str(player))
@@ -27,8 +27,10 @@ class Player(commands.Cog):
             if not data:
                 await ctx.send("Sorry, that player could not be found. Perhaps you made a typo?")
             else:
-                embed=discord.Embed(title="Player statistics for " + str(data["name"]), url="https://account.nethergames.org/player/" + str(player))
-                embed.set_author(name="NetherGames Network", url="https://nethergames.org", icon_url="https://cdn.nethergames.org/img/logo_small_trans.png")
+                embed = discord.Embed(title="Player statistics for " + str(data["name"]),
+                                      url="https://account.nethergames.org/player/" + str(player))
+                embed.set_author(name="NetherGames Network", url="https://nethergames.org",
+                                 icon_url="https://cdn.nethergames.org/img/logo_small_trans.png")
                 embed.set_thumbnail(url="https://player.nethergames.org/avatar/" + str(player))
                 embed.add_field(name="Kills", value=str(data["kills"]), inline=True)
                 embed.add_field(name="Deaths", value=str(data["deaths"]), inline=True)
@@ -45,15 +47,18 @@ class Player(commands.Cog):
                 embed.add_field(name="First seen", value=str(data["firstJoin"]), inline=True)
                 embed.add_field(name="Last seen", value=str(data["lastJoin"]), inline=True)
                 if int(data["online"]) == 1:
-                    embed.set_footer(text="Online - playing on " + str(data["lastServer"]), icon_url="https://cdn.nethergames.org/img/green.png")
+                    embed.set_footer(text="Online - playing on " + str(data["lastServer"]),
+                                     icon_url="https://cdn.nethergames.org/img/green.png")
                 else:
-                    embed.set_footer(text="Offline - last seen on " + str(data["lastServer"]), icon_url="https://cdn.nethergames.org/img/red.png")
+                    embed.set_footer(text="Offline - last seen on " + str(data["lastServer"]),
+                                     icon_url="https://cdn.nethergames.org/img/red.png")
                 try:
                     await ctx.send(embed=embed)
                 except BaseException as e:
                     await ctx.send("Backtrace: " + str(e))
         else:
             await ctx.send("Usage: ng!p <player>")
+
 
 def setup(bot):
     bot.add_cog(Player(bot))
